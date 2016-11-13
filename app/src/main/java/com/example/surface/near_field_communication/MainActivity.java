1e8dd424e6e1446dafafb59d77b3e4ea44c7c380
patrick ryan <pattyboy_07@hotmail.com>
2016-11-13 15:47:07 +1300

    added writing capabilities

--- app/src/main/java/com/example/surface/near_field_communication/MainActivity.java
+++ app/src/main/java/com/example/surface/near_field_communication/MainActivity.java
@@ -5,6 +5,7 @@ import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.AsyncTask;
 import android.os.Looper;
 import android.os.Message;
@@ -21,8 +22,10 @@ import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
 import android.os.Handler;
 
+import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.logging.LogRecord;
 
 
@@ -69,10 +72,12 @@ public class MainActivity extends AppCompatActivity {
         // check current state of a Switch (true or false).
         isWriting = simpleSwitch.isChecked();
 
-        //myTextView.setText("reading");
+
         if(isWriting){
-            handleWriting(intent);
             myTextView.setText("Writing");
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            handleWriting(intent, tag);
+
             return;
         }
 
@@ -102,8 +107,60 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void handleWriting(Intent intent){
+    private void handleWriting(Intent intent, Tag tag){
+        writeTag(tag, intent);
+    }
 
+    public NdefMessage createTextMessage(String content) {
+        try {
+            // Get UTF-8 byte
+            byte[] lang = Locale.getDefault().getLanguage().getBytes("UTF-8");
+            byte[] text = content.getBytes("UTF-8"); // Content in UTF-8
+
+            int langSize = lang.length;
+            int textLength = text.length;
+
+            ByteArrayOutputStream payload = new ByteArrayOutputStream(1 + langSize + textLength);
+            payload.write((byte) (langSize & 0x1F));
+            payload.write(lang, 0, langSize);
+            payload.write(text, 0, textLength);
+            NdefRecord record = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                    NdefRecord.RTD_TEXT, new byte[0],
+                    payload.toByteArray());
+            return new NdefMessage(new NdefRecord[]{record});
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void writeTag(Tag tag, Intent i)  {
+        NdefMessage message = createTextMessage("Chris Chris Chris");
+        if (tag != null) {
+            try {
+                Ndef ndefTag = Ndef.get(tag);
+                if (ndefTag == null) {
+                    // Let's try to format the Tag in NDEF
+                    NdefFormatable nForm = NdefFormatable.get(tag);
+                    if (nForm != null) {
+                        nForm.connect();
+                        nForm.format(message);
+                        nForm.close();
+                    }
+                }
+                else {
+                    ndefTag.connect();
+                    ndefTag.writeNdefMessage(message);
+                    ndefTag.close();
+                }
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        myTextView.setText("Written");
     }
 
     @Override
